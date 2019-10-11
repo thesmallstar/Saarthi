951a6c5699e2938c89dcbb3d76190d13745e1b45
thesmallstar <42006277+thesmallstar@users.noreply.github.com>
2019-10-12 02:14:20 +0530

    complete backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
+var bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+var User = require("./models/User");
+
 //Start routes
 router.post("/login", function(req, res) {
   res.send("In will let the user login");
 });
 
 router.post("/register", function(req, res) {
-  res.send("I will register the User");
+  res.send("I will be used to register the user.");
 });
 
 router.post("/verify", function(req, res) {
